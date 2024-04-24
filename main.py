42738d7de2384ec9935a93ca2dfb0f3232212c9a
Artemiy Mosin <artemymosin@yandex.ru>
2024-04-24 10:52:27 +0300

    add main file

---
+++ main.py
@@ -0,0 +1,150 @@
+import os
+import shutil
+import configparser
+
+class FileManager:
+    def __init__(self):
+        self.config = configparser.ConfigParser()
+        self.config.read("config.ini")
+        self.work_dir = self.config["DEFAULT"]["work_dir"]
+        os.chdir(self.work_dir)  # Переходим в рабочую директорию
+
+    def create_directory(self, dir_name):
+        try:
+            os.mkdir(dir_name)
+            print(f"Директория '{dir_name}' создана.")
+        except FileExistsError:
+            print(f"Директория '{dir_name}' уже существует.")
+
+    def delete_directory(self, dir_name):
+        try:
+            os.rmdir(dir_name)
+            print(f"Директория '{dir_name}' удалена.")
+        except FileNotFoundError:
+            print(f"Директория '{dir_name}' не найдена.")
+        except OSError:
+            print(f"Невозможно удалить директорию '{dir_name}'. Директория не пуста.")
+
+    def list_directory(self):
+        print("Содержимое текущей директории:")
+        for item in os.listdir():
+            print(item)
+
+    def change_directory(self, dir_name):
+        try:
+            os.chdir(dir_name)
+            print(f"Перешли в директорию '{dir_name}'.")
+        except FileNotFoundError:
+            print(f"Директория '{dir_name}' не найдена.")
+
+    def create_file(self, file_name):
+        try:
+            with open(file_name, "x"):
+                print(f"Файл '{file_name}' создан.")
+        except FileExistsError:
+            print(f"Файл '{file_name}' уже существует.")
+
+    def read_file(self, file_name):
+        try:
+            with open(file_name, "r") as file:
+                content = file.read()
+                print(f"Содержимое файла '{file_name}':")
+                print(content)
+        except FileNotFoundError:
+            print(f"Файл '{file_name}' не найден.")
+
+    def write_file(self, file_name, content):
+        try:
+            with open(file_name, "w") as file:
+                file.write(content)
+                print(f"Содержимое записано в файл '{file_name}'.")
+        except FileNotFoundError:
+            print(f"Файл '{file_name}' не найден.")
+
+    def delete_file(self, file_name):
+        try:
+            os.remove(file_name)
+            print(f"Файл '{file_name}' удален.")
+        except FileNotFoundError:
+            print(f"Файл '{file_name}' не найден.")
+
+    def copy_file(self, src_file, dest_file):
+        try:
+            shutil.copy2(src_file, dest_file)
+            print(f"Файл '{src_file}' скопирован в '{dest_file}'.")
+        except FileNotFoundError:
+            print(f"Файл '{src_file}' не найден.")
+
+    def move_file(self, src_file, dest_file):
+        try:
+            shutil.move(src_file, dest_file)
+            print(f"Файл '{src_file}' перемещен в '{dest_file}'.")
+        except FileNotFoundError:
+            print(f"Файл '{src_file}' не найден.")
+
+    def rename_file(self, old_name, new_name):
+        try:
+            os.rename(old_name, new_name)
+            print(f"Файл '{old_name}' переименован в '{new_name}'.")
+        except FileNotFoundError:
+            print(f"Файл '{old_name}' не найден.")
+
+# Пример использования
+if __name__ == "__main__":
+    file_manager = FileManager()
+    while True:
+        print("\nСписок доступных команд:")
+        print("1. Создать директорию")
+        print("2. Удалить директорию")
+        print("3. Просмотреть содержимое текущей директории")
+        print("4. Перейти в другую директорию")
+        print("5. Создать файл")
+        print("6. Прочитать содержимое файла")
+        print("7. Записать в файл")
+        print("8. Удалить файл")
+        print("9. Скопировать файл")
+        print("10. Переместить файл")
+        print("11. Переименовать файл")
+        print("12. Выйти")
+        choice = input("Выберите действие: ")
+        if choice == "1":
+            dir_name = input("Введите имя новой директории: ")
+            file_manager.create_directory(dir_name)
+        elif choice == "2":
+            dir_name = input("Введите имя директории для удаления: ")
+            file_manager.delete_directory(dir_name)
+        elif choice == "3":
+            file_manager.list_directory()
+        elif choice == "4":
+            dir_name = input("Введите имя директории: ")
+            file_manager.change_directory(dir_name)
+        elif choice == "5":
+            file_name = input("Введите имя нового файла: ")
+            file_manager.create_file(file_name)
+        elif choice == "6":
+            file_name = input("Введите имя файла для чтения: ")
+            file_manager.read_file(file_name)
+        elif choice == "7":
+            file_name = input("Введите имя файла для записи: ")
+            content = input("Введите содержимое для записи: ")
+            file_manager.write_file(file_name, content)
+        elif choice == "8":
+            file_name = input("Введите имя файла для удаления: ")
+            file_manager.delete_file(file_name)
+        elif choice == "9":
+            src_file = input("Введите имя исходного файла: ")
+            dest_file = input("Введите имя файла для копирования: ")
+            file_manager.copy_file(src_file, dest_file)
+        elif choice == "10":
+            src_file = input("Введите имя исходного файла: ")
+            dest_file = input("Введите имя файла для перемещения: ")
+            file_manager.move_file(src_file, dest_file)
+        elif choice == "11":
+            old_name = input("Введите старое имя файла: ")
+            new_name = input("Введите новое имя файла: ")
+            file_manager.rename_file(old_name, new_name)
+        elif choice == "12":
+            print("Выход.")
+            break
+        else:
+            print("Некорректный ввод. Пожалуйста, выберите действие снова.")
